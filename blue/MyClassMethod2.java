1e5042d4e02997ad07bc39c509ce4a03371c01d4
System Administrator <root@bluedeMacBook-Pro.local>
2019-06-19 17:05:38 +0800

    增加

---
+++ blue/MyClassMethod2.java
@@ -0,0 +1,16 @@
+package lessons;
+
+public class MyClassMethod2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyClassStudent Blue = new MyClassStudent();
+		Blue.setId(1);
+		Blue.setName("Blue");
+		Blue.setAge(18);
+		
+		System.out.println(Blue.name + " is " +Blue.age + " years old");
+		
+	}
+
+}
