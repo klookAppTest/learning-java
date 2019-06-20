bdd6ca6ecb496ca163c7b90f47210512e7b1dad3
blue786 <blue.xie@klook.com>
2019-06-20 21:16:54 +0800

    新增练习代码

---
+++ blue/MyClassCube2.java
@@ -0,0 +1,22 @@
+package lessons;
+
+public class MyClassCube2 {
+
+	int length;
+	int width;
+	int height;
+	
+	public int getCubeVolum() {
+		
+		return (length + width +height);
+		
+	}
+	
+	MyClassCube2(){
+		length = 10;
+		width = 20;
+		height = 30;
+		
+	}
+
+}
---
+++ blue/MyClassGouZaoCube.java
@@ -0,0 +1,11 @@
+package lessons;
+
+public class MyClassGouZaoCube {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyClassCube2 c1 = new MyClassCube2();
+		System.out.println(c1.getCubeVolum());
+	}
+
+}
